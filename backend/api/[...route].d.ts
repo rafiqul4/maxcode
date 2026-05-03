e796926d501bb943a5b557e425027d90e73b03a7
Rafiqul Islam <rafiqul.islam4@northsouth.edu>
2026-05-03 19:31:47 +0600

    chore(deploy): ignore generated backend api artifacts

--- backend/api/[...route].d.ts
+++
@@ -1,8 +0,0 @@
-export declare const runtime = "nodejs";
-export declare const GET: (req: Request) => Response | Promise<Response>;
-export declare const POST: (req: Request) => Response | Promise<Response>;
-export declare const PUT: (req: Request) => Response | Promise<Response>;
-export declare const PATCH: (req: Request) => Response | Promise<Response>;
-export declare const DELETE: (req: Request) => Response | Promise<Response>;
-export declare const OPTIONS: (req: Request) => Response | Promise<Response>;
-//# sourceMappingURL=%5B...route%5D.d.ts.map
